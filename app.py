1d135d815e5dd55e0549432edfbe1d1b3dacc0cc
victoria bottali <victoriab@sca-corp.com>
2019-11-22 09:53:04 -0500

    Some rearranging

--- app.py
+++ app.py
@@ -12,18 +12,15 @@ def index():
     return render_template("main.html")
 
 # Registration Page
-# Click 'New User'
+# Click 'New User' - Left Side
 @app.route('/Register')
 def register():
-    render_template("register.html")
-    return 'ok'
+    return render_template("register.html")
 
-@app.route('/User', methods=['POST'])
-def saveUser():
-    return render_template("NewUser.html")
-
-@app.route('/adduser',methods = ['POST', 'GET'])
-def adduser():
+# Called from the form in register.html on submit
+# Inserts the user into the database
+@app.route('/AddUser', methods = ['POST'])
+def addUser():
    print("test")
    if request.method == 'POST':
       try:
@@ -33,7 +30,7 @@ def adduser():
          
          with sqlite3.connect("Siip.db") as con:
             cur = con.cursor()
-            cur.execute("INSERT INTO users(name,phone) VALUES ('" + nm + "','" + phone + "');")
+            cur.execute("INSERT INTO users(name,phone,creditCard) VALUES (?,?,?)", (nm, phone, card))
             
             con.commit()
       except:
@@ -45,18 +42,18 @@ def adduser():
          return "success 2"
 
 # Scanner call - jQuery Ajax call 
+# Click 'Scan' - Right Side
 @app.route('/Scan')
 def scan():
       # Do scanner stuff here
       # Get userId - SQLLite
+      # return userInfo as JSON
       # redirect_to user/<userId> -- In front end
       return jsonify(results = {"dataKey": "value"})
 
 # User Page
 # For getting an existing user
-'''
 @app.route('/User/<userId>')
 def user(userId):
       return render_template("user.html", user=userId)
-'''
     
