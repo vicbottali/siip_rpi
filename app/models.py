dcaff873186f48ec840befd40fff2cecd0ffb1b1
victoria bottali <victoriab@sca-corp.com>
2019-11-26 13:29:38 -0500

    Adding sqlalchemy and migrate for db management

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from app import db 
+
+class User(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    phone = db.Column(db.String(120), index=True, unique=True)
+    creditCard = db.Column(db.String(120))
+    rfid = db.Column(db.String(128))
+
+    def __repr__(self):
+        return 'User: {}'.format(self.name)  
